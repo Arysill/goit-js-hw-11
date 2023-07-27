59bf94740118a1091ba6a9295728b1d9b980a039
Arysill <125421389+Arysill@users.noreply.github.com>
2023-07-27 00:49:08 -0500

    kugyjfd

--- src/fetch-images.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default async function fetchImages(value, page) {
-  const url = 'https://pixabay.com/api/';
-  const key = '38470230-70725d5e7b82f3a299dcdeaae';
-  const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-
-  return await axios.get(`${url}${filter}`).then(response => response.data);
-}
